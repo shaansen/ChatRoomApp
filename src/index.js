149d0bf269d1036dac3ced92392bd480cfcb8238
shaansen <shantanu.sengupta2008@gmail.com>
2019-06-16 18:17:03 -0400

    Code cleanup

--- src/index.js
+++ src/index.js
@@ -4,18 +4,14 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import createSagaMiddleware from "redux-saga";
 
-import "./index.css";
 import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
 import setupSocket from "./sockets";
 import reducers from "./reducers";
 import handleNewMessage from "./sagas";
 import username from "./utils/name";
 
 const sagaMiddleware = createSagaMiddleware();
-
 const store = createStore(reducers, applyMiddleware(sagaMiddleware));
-
 const socket = setupSocket(store.dispatch, username);
 
 sagaMiddleware.run(handleNewMessage, { socket, username });
@@ -26,4 +22,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
-registerServiceWorker();
